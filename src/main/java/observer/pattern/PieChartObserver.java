ee352c49942b302764cde12f60e86318e004b6aa
Rémi Grimal <remi.grimal@etu.univ-tlse2.fr>
2020-12-08 17:15:12 +0100

    fix #1.2: Add Pie Chart in nonPattern

---
+++ src/main/java/observer/pattern/PieChartObserver.java
@@ -0,0 +1,92 @@
+package observer.pattern;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.Vector;
+
+import javax.swing.JPanel;
+import observer.CourseRecord;
+import observer.LayoutConstants;
+
+/**
+ * This class represents a bar chart view of a vector of data. Uses the Observer
+ * pattern.
+ */
+@SuppressWarnings("serial")
+public class PieChartObserver extends JPanel implements Observer {
+	/**
+	 * Creates a BarChartObserver object
+	 * 
+	 * @param data
+	 *            a CourseData object to observe
+	 */
+	public PieChartObserver(CourseData data) {
+		data.attach(this);
+		this.courseData = data.getUpdate();
+		this.setPreferredSize(new Dimension(2 * LayoutConstants.xOffset
+				+ (LayoutConstants.barSpacing + LayoutConstants.barWidth)
+				* this.courseData.size(), LayoutConstants.graphHeight + 2
+				* LayoutConstants.yOffset));
+		this.setBackground(Color.white);
+	}
+
+	/**
+	 * Paint method
+	 * 
+	 * @param g
+	 *            a Graphics object on which to paint
+	 */
+	public void paint(Graphics g) {
+		super.paint(g);
+		int radius = 100;
+		
+		Integer[] data = new Integer[courseData.size()];
+
+		//first compute the total number of students
+		double total = 0.0;
+		for (int i = 0; i < data.length; i++) {
+			data[i] = courseData.get(i).getNumOfStudents();
+			total += data[i];
+		}
+		double startAngle = 0.0;
+		for (int i = 0; i < courseData.size(); i++) {
+			CourseRecord record = (CourseRecord) courseData.elementAt(i);
+			g.setColor(LayoutConstants.courseColours[i]);
+			double ratio = (data[i] / total) * 360.0;
+			g.fillArc(350, 50, 2 * radius, 2 * radius, (int) startAngle, (int) ratio);
+			startAngle += ratio;
+			g.drawString(courseData.get(i).getName(),
+					LayoutConstants.xOffset + (i + 1)
+							* LayoutConstants.barSpacing + i
+							* LayoutConstants.barWidth, LayoutConstants.yOffset
+							+ LayoutConstants.graphHeight + 20);
+			g.setColor(LayoutConstants.courseColours[i]);
+			g.drawString(record.getName(),
+					LayoutConstants.xOffset + (i + 1)
+							* LayoutConstants.barSpacing + i
+							* LayoutConstants.barWidth, LayoutConstants.yOffset
+							+ LayoutConstants.graphHeight + 20);
+		}
+	}
+
+	/**
+	 * Informs this observer that the observed CourseData object has changed
+	 * 
+	 * @param o
+	 *            the observed CourseData object that has changed
+	 */
+	public void update(Observable o) {
+		CourseData data = (CourseData) o;
+		this.courseData = data.getUpdate();
+
+		this.setPreferredSize(new Dimension(2 * LayoutConstants.xOffset
+				+ (LayoutConstants.barSpacing + LayoutConstants.barWidth)
+				* this.courseData.size(), LayoutConstants.graphHeight + 2
+				* LayoutConstants.yOffset));
+		this.revalidate();
+		this.repaint();
+	}
+
+	private Vector<CourseRecord> courseData;
+}
